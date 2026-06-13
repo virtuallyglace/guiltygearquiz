83c539f95bbcbf79954cac15d902be556880d98d
virtuallyglace <121405255+virtuallyglace@users.noreply.github.com>
2026-06-13 16:11:22 -0700

    Modify description of 'rewarding' question
    Updated the description for the 'rewarding' question.

--- src/xrd_quiz/questions.cljs
+++ src/xrd_quiz/questions.cljs
@@ -10,7 +10,7 @@
      [:specialist "Doing one thing and doing it well."]
      [:prepared "Having tools for every situation."]
      [:emotional "How well I emotionally connect with my character."]
-     [:rewarding "Aesthetics/feeling suitably rewarded when I perform well."]
+     [:rewarding "Aesthetics/feeling suitably rewarded when I perform well. grah"]
      ]
     :result :playstyle
     }
